d9a40ff2bc50881f4f0b81cadad56216bf7adbbf
Coder-pat <ensign224@gmail.com>
2026-04-24 01:39:59 +0000

    made react setup and structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+const name = 'Patricia'
+const age = 24
+  return (
+    <>
+      <h1>Hello, my name is {name}!</h1>
+      <p>I am {age} years old.</p>
+    </>
+  )
+}
+
+export default App
