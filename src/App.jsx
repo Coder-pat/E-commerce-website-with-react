e81b87d3abe3b6a638634ffc6879837b9bf5bfdc
Coder-pat <ensign224@gmail.com>
2026-05-02 12:52:41 +0000

    Added Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import { Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import Checkout from "./pages/Checkout";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="app">
+      <Navbar />
       <Routes>
-        <Route path="/" />
-        <Route path="/auth" />
-        <Route path="/checkout" />
+        <Route path="/" element={<Home />}/>
+        <Route path="/auth" element={<Auth />}/>
+        <Route path="/checkout" element={<Checkout />}/>
       </Routes>
     </div>
   );
